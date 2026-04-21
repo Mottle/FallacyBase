3a2df471cad62825ad46902717d3dcffde1f62ad
liar <yyxxyyzero@163.com>
2026-04-21 12:34:26 +0800

    feat(base): add event handler for mod construction and register attachments

--- src/main/kotlin/dev/deepslate/fallacy/base/ModAttachments.kt
+++ src/main/kotlin/dev/deepslate/fallacy/base/ModAttachments.kt
@@ -1,10 +1,14 @@
 package dev.deepslate.fallacy.base
 
 import dev.deepslate.fallacy.base.trait.TraitContainer
+import net.neoforged.bus.api.SubscribeEvent
+import net.neoforged.fml.common.EventBusSubscriber
+import net.neoforged.fml.event.lifecycle.FMLConstructModEvent
 import net.neoforged.neoforge.attachment.AttachmentType
 import net.neoforged.neoforge.registries.DeferredHolder
 import net.neoforged.neoforge.registries.DeferredRegister
 import net.neoforged.neoforge.registries.NeoForgeRegistries
+import thedarkcolour.kotlinforforge.neoforge.forge.MOD_BUS
 
 object ModAttachments {
     @JvmStatic
@@ -15,4 +19,14 @@ object ModAttachments {
         registry.register("behaviors") { _ ->
             AttachmentType.builder(TraitContainer::empty).serialize(TraitContainer.CODEC).copyOnDeath().build()
         }
+
+    @EventBusSubscriber(modid = TheMod.ID)
+    object RegisterHandler {
+        @SubscribeEvent
+        fun onModConstructed(event: FMLConstructModEvent) {
+            event.enqueueWork {
+                registry.register(MOD_BUS)
+            }
+        }
+    }
 }
